b33043338ce8bc5dc35d84f0191cc9b71645cc6c
Qinghui Liu <samleoqh@gmail.com>
2016-09-25 18:11:59 +0200

    initial docs

---
+++ Scoreboard/SDK/scorebd/Debug/src/kbDecode.d
@@ -0,0 +1,9 @@
+src/kbDecode.d: ../src/kbDecode.c ../src/kbDecode.h \
+ ../../standalone_bsp_0/microblaze_0/include/xio.h \
+ ../../standalone_bsp_0/microblaze_0/include/xbasic_types.h
+
+../src/kbDecode.h:
+
+../../standalone_bsp_0/microblaze_0/include/xio.h:
+
+../../standalone_bsp_0/microblaze_0/include/xbasic_types.h:
---
+++ Scoreboard/SDK/scorebd/Debug/src/main.d
@@ -0,0 +1,79 @@
+src/main.d: ../src/main.c \
+ ../../standalone_bsp_0/microblaze_0/include/xio.h \
+ ../../standalone_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xbasic_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+ ../../standalone_bsp_0/microblaze_0/include/xstatus.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_exception.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_types.h \
+ ../../standalone_bsp_0/microblaze_0/include/xintc.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_assert.h \
+ ../../standalone_bsp_0/microblaze_0/include/xparameters.h \
+ ../../standalone_bsp_0/microblaze_0/include/xstatus.h \
+ ../../standalone_bsp_0/microblaze_0/include/xintc_l.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_io.h \
+ ../../standalone_bsp_0/microblaze_0/include/mb_interface.h \
+ ../../standalone_bsp_0/microblaze_0/include/xil_printf.h \
+ ../../standalone_bsp_0/microblaze_0/include/mb_interface.h \
+ ../../standalone_bsp_0/microblaze_0/include/xgpio.h \
+ ../../standalone_bsp_0/microblaze_0/include/xgpio_l.h \
+ ../../standalone_bsp_0/microblaze_0/include/xtft.h \
+ ../../standalone_bsp_0/microblaze_0/include/xtft_hw.h \
+ ../../standalone_bsp_0/microblaze_0/include/xps2.h \
+ ../../standalone_bsp_0/microblaze_0/include/xps2_l.h \
+ ../../standalone_bsp_0/microblaze_0/include/xtmrctr_l.h \
+ ../../standalone_bsp_0/microblaze_0/include/xtmrctr.h \
+ ../../standalone_bsp_0/microblaze_0/include/xtmrctr_l.h \
+ ../src/kbDecode.h
+
+../../standalone_bsp_0/microblaze_0/include/xio.h:
+
+../../standalone_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/xstatus.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_exception.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_types.h:
+
+../../standalone_bsp_0/microblaze_0/include/xintc.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_assert.h:
+
+../../standalone_bsp_0/microblaze_0/include/xparameters.h:
+
+../../standalone_bsp_0/microblaze_0/include/xstatus.h:
+
+../../standalone_bsp_0/microblaze_0/include/xintc_l.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_io.h:
+
+../../standalone_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../standalone_bsp_0/microblaze_0/include/xil_printf.h:
+
+../../standalone_bsp_0/microblaze_0/include/mb_interface.h:
+
+../../standalone_bsp_0/microblaze_0/include/xgpio.h:
+
+../../standalone_bsp_0/microblaze_0/include/xgpio_l.h:
+
+../../standalone_bsp_0/microblaze_0/include/xtft.h:
+
+../../standalone_bsp_0/microblaze_0/include/xtft_hw.h:
+
+../../standalone_bsp_0/microblaze_0/include/xps2.h:
+
+../../standalone_bsp_0/microblaze_0/include/xps2_l.h:
+
+../../standalone_bsp_0/microblaze_0/include/xtmrctr_l.h:
+
+../../standalone_bsp_0/microblaze_0/include/xtmrctr.h:
+
+../../standalone_bsp_0/microblaze_0/include/xtmrctr_l.h:
+
+../src/kbDecode.h:
